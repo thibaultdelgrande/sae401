9cc0ce977532e7d1b3d43864696711bf23306e55
GuilMauv <guillaume.metz@uha.fr>
2023-04-06 15:16:16 +0200

    :bricks: mise des escape game + séparations des fichiers js entre le carrousel et le reste

--- src/Controller/AdventureController.php
+++ src/Controller/AdventureController.php
@@ -5,14 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\EscapeGame;
+use App\Repository\EscapeGameRepository;
 
 class AdventureController extends AbstractController
 {
     #[Route('/adventure', name: 'app_adventure')]
-    public function index(): Response
+    public function index(EscapeGameRepository $escapeGameRepository): Response
     {
+        $escapeGames = $escapeGameRepository->findAll();
         return $this->render('adventure/index.html.twig', [
             'controller_name' => 'AdventureController',
+            'escapeGames' => $escapeGames
         ]);
     }
 }
