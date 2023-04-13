72f27d49f6f3e7b091ffe323e9aa75cf5434f216
GuilMauv <guillaume.metz@uha.fr>
2023-04-13 09:20:02 +0200

    :bricks: création de toutes les pages

--- public/js/kaiserstuhl.js
+++ public/js/kaiserstuhl.js
@@ -1,8 +1,30 @@
-console.log("test");
-
-const event = document.querySelectorAll(".escape-individual").forEach(ev);
-event.addEventListener("click", Escapedesc);
+console.log("select");
+let i=0
 
 function Escapedesc(){
-    console.log(event);
+    const event = document.querySelectorAll(".escape-individual").forEach(el => {
+        el.classList.remove("selec");
+    });
+    if (i>0){
+        document.querySelector(".mask[data-id=\""+esc+"\"]").classList.remove("show");
+    }
+    var select = this.dataset.id;
+    console.log(select);
+    this.classList.add("selec");
+    document.querySelector(".mask[data-id=\""+select+"\"]").classList.add("show");
+    i++;
+    esc= select;
+    console.log(i);
+
 }
+
+const event = document.querySelectorAll(".escape-individual").forEach(el => {
+    el.addEventListener("click", Escapedesc);
+});
+
+var map = L.map('map').setView([48.037310786182196, 7.642389084893441], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+var marker = L.marker([48.037310786182196, 7.642389084893441]).addTo(map);
