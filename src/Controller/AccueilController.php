af38553e5cb9019e1f73c82d3a02ffa909f6d9b4
Thibault Delgrande <thib@ultdelgran.de>
2023-04-13 15:21:11 +0200

    ✨ Add translation

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -6,16 +6,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\EscapeGameRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class AccueilController extends AbstractController
 {
     #[Route('/', name: 'app_accueil')]
-    public function index(EscapeGameRepository $escapeGameRepository): Response
-    {
+    public function index(EscapeGameRepository $escapeGameRepository, TranslatorInterface $translator, Request $request): Response
+    {        
         $escapeGames = $escapeGameRepository->findAll();
+
+        $locale = $request->get('lang', $request->getLocale());
+        $translator->setLocale($locale);
+
+        $url = $request->getPathInfo() . '?lang=' . $locale;
+
         return $this->render('accueil/index.html.twig', [
             'controller_name' => 'AccueilController',
-            'escapeGames' => $escapeGames
+            'escapeGames' => $escapeGames,
+            'url' => $url,
         ]);
     }
 }
