57fe28e45ab8c58494c416c7b05d911e587bd669
GuilMauv <guillaume.metz@uha.fr>
2023-04-13 10:52:59 +0200

    :bricks: modification et creation des gift cards

--- src/Controller/AdventureController.php
+++ src/Controller/AdventureController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\EscapeGame;
 use App\Repository\EscapeGameRepository;
 
 class AdventureController extends AbstractController
--- src/Controller/FaqController.php
+++ src/Controller/FaqController.php
@@ -5,14 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\FaqRepository;
 
 class FaqController extends AbstractController
 {
     #[Route('/faq', name: 'app_faq')]
-    public function index(): Response
+    public function index(FaqRepository $faqRepository): Response
     {
+        $faq=$faqRepository->findAll();
         return $this->render('faq/index.html.twig', [
             'controller_name' => 'FaqController',
+            'faqs' => $faq,
         ]);
     }
 }
