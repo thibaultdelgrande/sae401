19ee46daead35b4fd1a4f66f3cee60fb00f4d723
Thibault Delgrande <thib@ultdelgran.de>
2023-04-14 04:04:32 +0200

    🚀 Traduction de la FAQ

--- src/Form/FaqType.php
+++ src/Form/FaqType.php
@@ -21,7 +21,7 @@ class FaqType extends AbstractType
                     'placeholder' => 'Enter the question',
                     'class' => 'form-control',
                     'minlength' => '3',
-                    'maxlength' => '50'
+                    'maxlength' => '255'
                 ],
                 'label' => 'Question',
                 'label_attr' => [
@@ -45,7 +45,7 @@ class FaqType extends AbstractType
                     'placeholder' => 'Enter the answer to the question',
                     'class' => 'form-control',
                     'minlength' => '3',
-                    'maxlength' => '511'
+                    'maxlength' => '1023'
                 ],
                 'label' => 'Answer',
                 'label_attr' => [
@@ -60,10 +60,107 @@ class FaqType extends AbstractType
                         'min' => 3,
                         'minMessage' => 'Your description should be at least {{ limit }} characters',
                         // max length allowed by Symfony for security reasons
-                        'max' => 511,
+                        'max' => 1023,
                     ]),
                 ],
             ])
+            ->add('questionFR' , TextType::class ,[
+                'attr' => [
+                    'placeholder' => 'Enter the question in french',
+                    'class' => 'form-control',
+                    'minlength' => '3',
+                    'maxlength' => '255'
+                ],
+                'label' => 'Question in french',
+                'label_attr' => [
+                    'class' => 'form-label'
+                ],
+                'required' => true,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter question',
+                    ]),
+                    new Length([
+                        'min' => 3,
+                        'minMessage' => 'Your question should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 255,
+                    ]),
+                ],
+            ])
+            ->add('answerFR', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'Enter the answer to the question in french',
+                    'class' => 'form-control',
+                    'minlength' => '3',
+                    'maxlength' => '1023'
+                ],
+                'label' => 'Answer in french',
+                'label_attr' => [
+                    'class' => 'form-label'
+                ],
+                'required' => true,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter an answer',
+                    ]),
+                    new Length([
+                        'min' => 3,
+                        'minMessage' => 'Your description should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 1023,
+                    ]),
+                ],
+            ])
+            ->add('questionDE' , TextType::class ,[
+                'attr' => [
+                    'placeholder' => 'Enter the question in german',
+                    'class' => 'form-control',
+                    'minlength' => '3',
+                    'maxlength' => '255'
+                ],
+                'label' => 'Question in german',
+                'label_attr' => [
+                    'class' => 'form-label'
+                ],
+                'required' => true,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter question',
+                    ]),
+                    new Length([
+                        'min' => 3,
+                        'minMessage' => 'Your question should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 255,
+                    ]),
+                ],
+            ])
+            ->add('answerDE', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'Enter the answer to the question in german',
+                    'class' => 'form-control',
+                    'minlength' => '3',
+                    'maxlength' => '1023'
+                ],
+                'label' => 'Answer in german',
+                'label_attr' => [
+                    'class' => 'form-label'
+                ],
+                'required' => true,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter an answer',
+                    ]),
+                    new Length([
+                        'min' => 3,
+                        'minMessage' => 'Your description should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 1023,
+                    ]),
+                ],
+            ])
+
             ->add('submit', SubmitType::class, [
                 'attr' => [
                     'class' => 'btn btn-primary'
